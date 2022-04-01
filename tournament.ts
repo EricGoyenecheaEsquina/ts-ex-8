a9ab1ac8367ed481a081462a45d9d6af5e9c73ee
EricGoyenecheaEsquina <102578352+EricGoyenecheaEsquina@users.noreply.github.com>
2022-04-01 11:24:42 +0200

    mod tournament

--- tournament.ts
+++ tournament.ts
@@ -12,39 +12,53 @@ export class Tournament {
   public tally(input: string): string {
     let DevastatingDonkeys: Team = new Team("Devastating Donkeys");
     let AllegoricAlaskans: Team = new Team("Allegoric Alaskans");
-    let BlitheringBadgers: Team = new Team("Blithering Badgers");
-    let CourageousCalifornians: Team = new Team("Courageous Californians");
-    let teams: Team[] = [DevastatingDonkeys, AllegoricAlaskans, BlitheringBadgers, CourageousCalifornians]
     let result: string[] = input.replace(/(\r\n|\n|\r)/gm, "-").split("-");
     let header: string = 'Team                           | MP |  W |  D |  L |  P';
     let modified: Set<Team> = new Set();
     if (input != '') {
+      let teams: Team[]=[];
       for (let item of result) {
-        let resultUnic: string[] = item.split(";");
-        let nombre1: string = resultUnic[0];
-        let nombre2: string = resultUnic[1];
-        let score: string = resultUnic[2];
+        let [nombre1,nombre2,score] = item.split(";");
+        let crear1: Boolean=true;
+        let crear2: Boolean=true;
+        for(let item of teams){
+          if(item.nombre === nombre1){
+            crear1=false;
+          }
+          if(item.nombre === nombre2){
+            crear2=false;
+          }
+        }
         let equipo1 = DevastatingDonkeys;
         let equipo2 = AllegoricAlaskans;
+        if(crear1){
+          equipo1=new Team(nombre1);
+          teams.push(equipo1);
+        }
+        if(crear2){
+          equipo2=new Team(nombre2);
+          teams.push(equipo2);
+        }
+        
         for (let item of teams) {
-          if (item.nombre == nombre1) {
+          if (item.nombre === nombre1) {
             equipo1 = item;
             modified.add(equipo1);
           }
-          if (item.nombre == nombre2) {
+          if (item.nombre === nombre2) {
             equipo2 = item;
             modified.add(equipo2);
           }
         }
-        if (score == 'win') {
-          equipo1.ganados += 1;
-          equipo2.perdidos += 1;
-        } else if (score == 'draw') {
-          equipo1.empates += 1;
-          equipo2.empates += 1;
+        if (score === 'win') {
+          equipo1.ganados ++;
+          equipo2.perdidos ++;
+        } else if (score === 'draw') {
+          equipo1.empates ++;
+          equipo2.empates ++;
         } else {
-          equipo2.ganados += 1;
-          equipo1.perdidos += 1;
+          equipo2.ganados ++;
+          equipo1.perdidos ++;
         }
         equipo1.puntos= equipo1.ganados * 3 + equipo1.empates;
         equipo2.puntos= equipo2.ganados * 3 + equipo2.empates;
@@ -55,7 +69,7 @@ export class Tournament {
      });
       modifiedArray=modifiedArray.sort((a, b) =>{
         if (b.puntos > a.puntos) return 1;
-        if (b.puntos == a.puntos) return 0;
+        if (b.puntos === a.puntos) return 0;
         return -1;
      });
       for (let item of modifiedArray) {
